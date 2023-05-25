803e2de374ac25e9cff6cb7a0a1bad232fbdf2c8
Rohit-Karki <rohitkarki804@gmail.com>
2023-05-25 13:34:20 +0545

    Small Edits

--- admin/pages/index.jsx
+++ admin/pages/index.jsx
@@ -6,23 +6,23 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [error,setError] = useState("false")
-  const [loading,setLoading] = useState("true")
-  useEffect(() =>{
-    const responseShop = JSON.parse(localStorage.getItem('response'));
-    if(!response){
+  const [error, setError] = useState("false");
+  const [loading, setLoading] = useState("true");
+  useEffect(() => {
+    const responseShop = JSON.parse(localStorage.getItem("response"));
+    if (!response) {
       setError("true");
-    }else{
-    setLoading(false)
-    
+    } else {
+      setLoading(false);
+      
     }
-  },[loading])
-  if(error)
-  return (
-    <>
-      <Layout>
-        <Dashford />
-      </Layout>
-    </>
-  );
+  }, [loading]);
+  if (error)
+    return (
+      <>
+        <Layout>
+          <Dashford />
+        </Layout>
+      </>
+    );
 }
